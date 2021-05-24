5e8bfdd366f027b46d701cbf624b4c979ce509c0
DrillND <gornhub13@gmail.com>
2021-05-24 16:36:03 +0900

    1

---
+++ 20210524_1/src/com/hundai/carAPP/util/CarRandData.java
@@ -0,0 +1,43 @@
+package com.hundai.carAPP.util;
+
+import java.util.Random;
+
+public class CarRandData {
+	String name[] = { "홍길동", "김길동", "박길동", "이길동", "최길동" };
+	String tel[] = { "010-1234-5678", "010-4321-5678", "010-3478-1278", "010-4523-1978", "010-7890-2134" };
+	String address[] = { "대구시 동구 신암4동", "서울시 동구 신암4동", "부산시 동구 신암4동", "인천시 동구 신암4동", "광주시 동구 신암4동" };
+	String model[] = { "SM6", "쏘나타", "싼타페", "K7", "그랜져" };
+	String color[] = { "블랙", "은색", "흰색", "회색", "빨강" };
+	int year[] = { 2016, 2017, 2016, 2017, 2016 };
+	String company[] = { "삼성르노", "현대", "현대", "기아", "현대" };
+	
+	Random r = new Random();
+	
+	public String getName() {
+		return name[r.nextInt(5)];
+	}
+	
+	public String getTel() {
+		return tel[r.nextInt(5)];
+	}
+	
+	public String getAddress() {
+		return address[r.nextInt(5)];
+	}
+	
+	public String getModel() {
+		return model[r.nextInt(5)];
+	}
+	
+	public String getColor() {
+		return color[r.nextInt(5)];
+	}
+	
+	public int getYear() {
+		return year[r.nextInt(5)];
+	}
+	
+	public String getCompany() {
+		return company[r.nextInt(5)];
+	}
+}
