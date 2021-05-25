1af2e98d01dd6a94dc5c63e150f456b8a499e745
DrillND <gornhub13@gmail.com>
2021-05-25 15:36:57 +0900

    1

--- 20210524_1Paper2/src/com/hundai/carAPP/CarAppMain2.java
+++ 20210524_1Paper2/src/com/hundai/carAPP/CarAppMain2.java
@@ -1,25 +1,37 @@
 package com.hundai.carAPP;
 
+import java.util.Scanner;
+
+import com.hundai.carAPP.handler.CarSellMgr;
 import com.hundai.carAPP.view.Menu2;
 
 public class CarAppMain2 {
 
 	public static void main(String[] args) {
-		Menu2 m = new Menu2();
+		Scanner s = new Scanner(System.in);
+		Menu2 m = new Menu2(s); //싱글톤 개념
+		CarSellMgr cs = new CarSellMgr();
+		//객체생성
 		while(true) {
 			switch (m.mainMenu()){
-				case 1:
-					System.out.println("중고차 매매 현황 보기");
+				case Menu2.MAIN_MENU_VIEW:
+					cs.showSellInfor();
+					//System.out.println("중고차 매매 현황 보기");
 					break;
-				case 2:
-					System.out.println("중고차 매매 랜덤 자료 추가");
+				case Menu2.MAIN_MENU_RAND_INS:
+					cs.randInsData(m.getRandCnt());
+					//System.out.println("중고차 매매 랜덤 자료 추가");
 					break;
 				case 3:
 					System.out.println("중고차 매매 수동 자료 추가");
 					break;
 				case 4 :
-					System.out.println("프로그램 종료");
-					System.exit(0);
+					//System.out.println("프로그램 종료");
+					//System.exit(0);
+					cs.mainExit();
+				default:
+					System.out.println("올바른 메뉴를 다시 선택하세요.");
+					break;
 			}
 			
 		}
---
+++ 20210524_1Paper2/src/com/hundai/carAPP/handler/CarSellMgr.java
@@ -0,0 +1,43 @@
+//내부 기능 코딩
+package com.hundai.carAPP.handler;
+
+import java.util.ArrayList;
+
+import com.hundai.carAPP.car.Car;
+import com.hundai.carAPP.customer.Customer;
+import com.hundai.carAPP.util.CarRandData;
+
+public class CarSellMgr {
+	ArrayList<Customer> cuList = new ArrayList<Customer>();
+	CarRandData r = new CarRandData();
+
+	public void showSellInfor() {
+		for (Customer cu : cuList) {
+			cu.printCustomerInfo();
+		}
+
+		// for (int i =0; i<cuList.size(); i++) {
+		// cuList.get(i).printCustomerInfo();
+		// }
+	}
+
+	public void randInsData() {
+		cuList.add(new Customer(r.getName(), r.getTel(), r.getAddress(),
+				new Car(r.getModel(), r.getColor(), r.getYear(), r.getCompany())));
+	}
+
+	public void randInsData(int size) {
+		for (int i = 0; i < size; i++) {
+			cuList.add(new Customer(r.getName(), r.getTel(), r.getAddress(),
+					new Car(r.getModel(), r.getColor(), r.getYear(), r.getCompany())));
+		}
+	}
+
+	public void insData() {
+
+	}
+
+	public void mainExit() {
+		System.exit(0);
+	}
+}
